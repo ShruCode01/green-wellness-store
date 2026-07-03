3475450976971ffde76f7bd856357174548ab5d1
shruCode18 <your-email-used-in-github>
2026-07-03 23:19:25 +0530

    mobile responsive

--- src/Component/Sloggen.jsx
+++ src/Component/Sloggen.jsx
@@ -3,7 +3,7 @@ import '../Style/Sloggen.css'
 const Sloggen = () => {
   return (
     <div>
-        <h2 className='heading2 mt-5 text-center'>100% PURE & AUTHENTIC AYURVEDIC WELLNESS</h2>
+        <h2 className='heading2  text-center'>100% PURE & AUTHENTIC AYURVEDIC WELLNESS</h2>
         <p className='para'>शुद्धता ही सर्वोत्तम है</p>
         <p className='heading5'>Our products are prepared from pure Ayurvedic herbs and natural elements, promising to nourish your face and health.</p>
     </div>
