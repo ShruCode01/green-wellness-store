d33d0e136d1a5145049135d528c9df298244378c
shruCode18 <your-email-used-in-github>
2026-07-25 23:24:40 +0530

    ok

--- src/main.jsx
+++ src/main.jsx
@@ -9,8 +9,8 @@ import { CartProvider } from './Component/CartContext';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* <BrowserRouter basename="/green-wellness-store"> */}
-    {/* <BrowserRouter> */}
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <BrowserRouter>
+    {/* <BrowserRouter basename={import.meta.env.BASE_URL}> */}
       <CartProvider>
         <App />
       </CartProvider>
--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,15 @@
 // import { defineConfig } from 'vite'
 // import react from '@vitejs/plugin-react'
 // 
-// export default defineConfig({
-  // plugins: [react()],
-  // base: '/green-wellness-store/',
-// })
-
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   plugins: [react()],
-  base: mode === "production" ? "/green-wellness-store/" : "/",
-}));
+  base: '/green-wellness-store/',
+})
+
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// 
+// export default defineConfig(({ mode }) => ({
+  // plugins: [react()],
+  // base: mode === "production" ? "/green-wellness-store/" : "/",
+// }));
