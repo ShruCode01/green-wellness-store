0f05dd7146a73a03468de13683aa57daa9a3ebea
shruCode18 <your-email-used-in-github>
2026-07-25 23:34:55 +0530

    done

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 // 
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  base: '/green-wellness-store/',
-})
+// export default defineConfig({
+  // plugins: [react()],
+  // base: '/green-wellness-store/',
+// })
 
 // import { defineConfig } from "vite";
 // import react from "@vitejs/plugin-react";
@@ -14,3 +14,12 @@ export default defineConfig({
   // plugins: [react()],
   // base: mode === "production" ? "/green-wellness-store/" : "/",
 // }));
+
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  base: process.env.VERCEL ? "/" : "/green-wellness-store/",
+});
